08c2ff3c23d2cd5dab5f05a1d39af158194278e6
foma <fomenkoyegor@gmail.com>
2018-05-31 23:00:33 +0300

    norm server works without cors

---
+++ src/app/nav/nav.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { PostService } from '../post.service';
+import {Response} from '@angular/http';
+
+@Component({
+  selector: 'app-nav',
+  templateUrl: './nav.component.html',
+  styleUrls: ['./nav.component.scss']
+})
+export class NavComponent implements OnInit {
+  arr:any
+
+  constructor(public service:PostService) { }
+
+  ngOnInit() {
+    this.service.getPosts()
+    .subscribe(
+      (res:any) => this.arr = res,
+      (error: Response) => console.log(error),
+    );
+
+  }
+
+}
--- src/app/post.service.ts
+++ src/app/post.service.ts
@@ -1,8 +1,17 @@
 import { Injectable } from '@angular/core';
+import {Http} from '@angular/http';
+import 'rxjs/add/operator/map';
+import {Observable} from 'rxjs';
 
 @Injectable()
 export class PostService {
+  readonly URL_POSTS='http://localhost:3000/api/posts';
+  result:any;
+  constructor(private http:Http) { }
 
-  constructor() { }
+  getPosts(): Observable<any>{
+    return this.http.get(this.URL_POSTS)
+      .map(res=>this.result=res.json())
+  }
 
 }
