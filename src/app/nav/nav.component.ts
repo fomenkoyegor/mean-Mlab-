e177e327270ad8eac29a52633042a9791264b427
foma <fomenkoyegor@gmail.com>
2018-06-01 11:58:08 +0300

    add some

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,6 +1,5 @@
 import {Component, OnInit} from '@angular/core';
-import {PostService} from '../post.service';
-import {Response} from '@angular/http';
+
 
 @Component({
   selector: 'app-nav',
@@ -8,17 +7,12 @@ import {Response} from '@angular/http';
   styleUrls: ['./nav.component.scss']
 })
 export class NavComponent implements OnInit {
-  arr: any;
+
 
   constructor(public service: PostService) {
   }
 
   ngOnInit() {
-    this.service.getPosts()
-      .subscribe(
-        (res: any) => this.arr = res,
-        (error: Response) => console.log(error),
-      );
 
   }
 
